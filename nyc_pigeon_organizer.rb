3518403dcd4b0c2792ced25add8dc8a45253a3d0
MatteBru <mbruni616@gmail.com>
2017-09-19 17:30:39 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  require "pry"
+  new_hash ={}
+  data.each do |attrib, vals|
+    vals.each do |val, mems|
+      mems.each do |name|
+        new_hash[name] = {} if !new_hash.keys.include?(name)
+        new_hash[name][attrib] = [] if !new_hash[name].keys.include?(attrib)
+        new_hash[name][attrib] << val.to_s
+      end
+    end
+  end
+  new_hash
 end
