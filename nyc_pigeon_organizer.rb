ea7a4f21091b39ae67c54914f4f0948f24f0b091
Elisa Vaccaro <erv230@nyu.edu>
2017-10-07 16:09:47 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,25 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  name_holder = []
+  data[:gender].each do |gen, names|
+    names.each do |name|
+      name_holder.push(name)
+    end
+  end
+  name_holder.each do |name|
+    pigeon_list[name] = {}
+  end
+
+  data.each do |cat, subcat|
+    pigeon_list.each do |name, hash|
+    pigeon_list[name][cat] = []
+    subcat.each do |type, pigeons|
+      if pigeons.include?(name)
+        pigeon_list[name][cat].push(type.to_s)
+      end
+    end
+  end
+end
+    pigeon_list
+
 end
